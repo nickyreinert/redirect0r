7476d75df013350df6990f7c028e4404b0f02751
nickyreinert <mail@nickyreinert.de>
2017-10-13 18:23:12 +0200

    init

---
+++ run.php
@@ -0,0 +1,83 @@
+<?php
+
+	ob_start();
+	
+	$limit = 100000;
+	
+	$sleep = 2;
+	
+	$repeat = 5;
+	
+	$growHtaccess = TRUE;
+	
+	$chunks = 1000;
+	
+	$basePath = str_replace(basename(__FILE__), '', $_SERVER['REQUEST_URI']);
+	
+	$currentUrl = $_SERVER['REQUEST_SCHEME'].'://'.$_SERVER['SERVER_NAME'].$basePath;
+
+	$dummyPath = 'foobar';
+	
+	$htaccess = 'RewriteEngine On'. PHP_EOL;
+	
+	$countRows = 1;
+	
+	echo 'Start. <br/ ><br/ >';
+
+	
+	for ($i = 1; $i <= $limit; ++$i)
+	{	
+		if ($growHtaccess === TRUE)
+		{
+			
+			$htaccess .= 'Redirect 302 '.$basePath.$dummyPath.$i.'/'.' '.$currentUrl.'index.php'.PHP_EOL;
+			
+		} else {
+
+			$countRows	= 1;
+		
+			$htaccess = 'RewriteEngine On'. PHP_EOL;
+
+			$htaccess = 'Redirect 302 '.$basePath.$dummyPath.$i.'/'.' '.$currentUrl.'index.php'.PHP_EOL;
+		
+		}
+		
+		if ($i === 1) 
+		{
+			echo 'Redir-Rule: '. $htaccess . '<br />';
+			echo 'Test-URL: '.$currentUrl.$dummyPath.$i.'/ <br /><br />';
+			echo 'rows;delay [ms];sleep [s] <br />';
+
+	
+		}
+
+		
+		if ($i % round($limit / $chunks) === 0)
+		{
+			file_put_contents('.htaccess', $htaccess);
+			
+			for ($j = 1; $j <= $repeat; ++$j)
+			{
+				$startTime = microtime(true);
+				
+				$dummy = file_get_contents($currentUrl.$dummyPath.$i.'/');
+
+				$endTime = round(1000 * (microtime(true) - $startTime));
+				
+				echo str_pad('',4096);
+				ob_flush();
+				flush();
+				
+				echo $countRows.';'.$endTime.';'.$sleep.'<br />';
+				
+				sleep($sleep);
+				
+			}
+			
+		}
+		
+		++$countRows;
+		
+	}
+	
+	echo 'End.';
